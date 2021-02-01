792cfdc73bc72cf7b1090774aae00414e7b8c243
Arpit-spt <arpitc.spt@gmail.com>
2021-02-01 12:56:41 +0530

    if slot is before date it will not cancelled

--- src/main/java/com/Turfbooking/service/Impl/UserServiceImpl.java
+++ src/main/java/com/Turfbooking/service/Impl/UserServiceImpl.java
@@ -233,6 +233,9 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public TimeSlotResponse cancelBookedSlot(CancelOrUnavailableSlotRequest cancelRequest) throws GeneralException, RazorpayException {
+        if (cancelRequest.getDate().isBefore(LocalDate.now(ZoneId.of("Asia/Kolkata"))) /*|| cancelRequest.getDate().equals(LocalDate.now(ZoneId.of("Asia/Kolkata")))*/) {
+            throw new GeneralException("slot cannot be cancelled with date : " + cancelRequest.getDate().toString(), HttpStatus.BAD_REQUEST);
+        }
         BookedTimeSlot timeSlot = bookedTimeSlotRepository.findByTurfIdAndStartTimeAndDate(cancelRequest.getTurfId(), LocalDateTime.of(cancelRequest.getDate(), cancelRequest.getStartTime()), cancelRequest.getDate());
         if (null != timeSlot) {
             CancelledSlot cancelledSlot = new CancelledSlot(timeSlot);
