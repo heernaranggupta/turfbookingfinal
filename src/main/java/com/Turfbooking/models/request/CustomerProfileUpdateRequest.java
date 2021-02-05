66f05e4ab2ddf0719ba3a7e81637229359242c9c
Arpit-spt <arpitc.spt@gmail.com>
2021-02-05 12:07:33 +0530

    issue resolved

--- src/main/java/com/Turfbooking/models/request/CustomerProfileUpdateRequest.java
+++ src/main/java/com/Turfbooking/models/request/CustomerProfileUpdateRequest.java
@@ -2,29 +2,41 @@ package com.Turfbooking.models.request;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
+@Valid
 @Getter
+@Setter
 @Builder
 public class CustomerProfileUpdateRequest {
-    @NotNull
+    @NotNull(message = "name should be not null")
     private String name;
-    //    private String password;
-//    private String countryCode;
     @NotEmpty(message = "Please provide contact number")
     private String phoneNumber;
+    @NotNull(message = "gender should be not null")
     private String gender;
+    @NotNull(message = "dateOfBirth should be not null")
     private LocalDate dateOfBirth;
+    @NotNull(message = "addressLine should be not null")
     private String addressLine;
+    @NotNull(message = "zipCode should be not null")
     private String zipCode;
+    @NotNull(message = "city should be not null")
     private String city;
+    @NotNull(message = "state should be not null")
     private String state;
+    @NotNull(message = "latitude should be not null")
     private Double latitude;
+    @NotNull(message = "longitude should be not null")
     private Double longitude;
+    @NotNull(message = "emailId should be not null")
     private String emailId;
+    @NotNull(message = "downloadUrl should be not null")
     private String downloadUrl;
 
 }
