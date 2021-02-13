ad32ebf566f996051c6a756e2e695848cbb609ac
Arpit-spt <arpitc.spt@gmail.com>
2021-02-13 13:18:43 +0530

    cors allowed

--- src/main/java/com/Turfbooking/configuration/WebSecurityConfing.java
+++ src/main/java/com/Turfbooking/configuration/WebSecurityConfing.java
@@ -68,6 +68,10 @@ public class WebSecurityConfing extends WebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.applyPermitDefaultValues();
+        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
+                "Cache-Control", "Content-Type"));
+        configuration.addAllowedOrigin("*");
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
