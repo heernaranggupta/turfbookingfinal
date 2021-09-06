f8a67cea65e1ae286b7437d76b444882d2d654ee
Arpit-spt <arpitc.spt@gmail.com>
2021-09-06 15:29:46 +0530

    jar

--- src/main/java/com/Turfbooking/TurfbookingApplication.java
+++ src/main/java/com/Turfbooking/TurfbookingApplication.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -16,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableMongoRepositories
 @OpenAPIDefinition(info = @Info(title = "Turf Booking", version = "0.1", description = "API documentation of turf booking project."))
-public class TurfbookingApplication extends SpringBootServletInitializer {
+public class TurfbookingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TurfbookingApplication.class, args);
